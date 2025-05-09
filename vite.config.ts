49574004d1332f043c13447b91c06f034c4a8ba7
Gabriel Hollenstein <gabriel.hollenstein@vertigis.com>
2025-05-09 13:47:37 +0200

    add 404 Page. Remove http sites

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 👇 Set base to your repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/uselessweb/",
+  base: "/",
 });
